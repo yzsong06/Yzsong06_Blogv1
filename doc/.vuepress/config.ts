7bcf034d5c66cf0d44a903af44e0f6c5ffcd07d1
yzsong06 <yzsong06@outlook.com>
2023-06-19 02:57:37 +0000

    修正部署到子目录后出现的样式问题

--- doc/.vuepress/config.ts
+++ doc/.vuepress/config.ts
@@ -2,11 +2,10 @@ import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
 
 export default defineUserConfig({
-  base: "/",
-  dest: "./source/docs",
+  base: "/docs/",
+  dest: "./public/docs",
   lang: "zh-CN",
   title: "一只鬆的文档站",
-  
   theme,
 
   // Enable it with pwa
